ddae63b7d101fca863ab89dc6cf0364ebe80b0c3
dlawr <dlawr@10.71.4.113>
2021-04-09 22:35:46 -0400

    one

---
+++ MultiplicationAndRandomNumber/src/multiplicationAndRandomNumber/MultiplicationTable.java
@@ -0,0 +1,17 @@
+package multiplicationAndRandomNumber;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		for(int one = 1; one<=10; one++)
+		{
+			for(int two = 1; two<=10; two++)
+			{
+				System.out.printf("%5d", (one*two));
+			}
+			System.out.println();
+		}
+
+	}
+
+}
